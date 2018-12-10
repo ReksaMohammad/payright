a7f39cd031381de04c330a9d33917b3efd3c5d63
Reksa <42407562+ReksaMohammad@users.noreply.github.com>
2018-12-10 14:26:36 +0700

    update_v3

--- js/script.js
+++ js/script.js
@@ -35,27 +35,28 @@ $('#form_kontak').submit(function(e){
 	e.preventDefault();
 	var $form = $(this);
 	if (document.forms["name"].value == "") {
-		alert("Harap Masukan Nama Perusanaan Anda");
+		$.growl.warning({ message: "Harap Masukan Nama Anda" });
 	} else if ($form.find("select[name='jenis_perusahaan']>option:selected").val() == "0") {
-		alert("Harap Masukan Jenis Perusanaan Anda");
-	} else if (document.forms["phone"].value == "") {
-		alert("Harap Masukan No. Telp Anda");
+		$.growl.warning({ message: "Harap Masukan Ukuran Perusanaan Anda" });
+	} else if (document.forms["jabatan"].value == "") {
+		$.growl.warning({ message: "Harap Masukan Jabatan Anda" });
 	} else if (document.forms["email"].value == "") {
-		alert("Harap Masukan Email Anda");
+		$.growl.warning({ message: "Harap Masukan Email Anda" });
 	} else if (document.forms["kota"].value == "") {
-		alert("Harap Masukan Kota Asal Anda");
-	} else if ($form.find("select[name='provinsi']>option:selected").val() == "34") {
-		alert("Harap Masukan Provinsi Asal Anda");
+		$.growl.warning({ message: "Harap Masukan Kota Asal Anda" });
+	} else if (document.forms["phone"].value == "") {
+		$.growl.warning({ message: "Harap Masukan No. Telp Anda" });
 	} else {
 		name 						 = $form.find("input[name='name']").val();
 		jenis_perusahaan = $form.find("select[name='jenis_perusahaan']>option:selected").val();
 		phone 					 = $form.find("input[name='phone']").val();
 		email 					 = $form.find("input[name='email']").val();
 		kota 						 = $form.find("input[name='kota']").val();
-		provinsi 				 = $form.find("select[name='provinsi']>option:selected").val();
+		jabatan 				 = $form.find("input[name='jabatan']").val();
+		pesan  					 = $form.find("input[name='pesan']").val();
 		url 						 = $form.attr("action");
 
-		var posting		 = $.post(url, {name: name, jenis_perusahaan: jenis_perusahaan, phone: phone, email: email, kota: kota, provinsi: provinsi});
+		var posting		 = $.post(url, {name: name, jenis_perusahaan: jenis_perusahaan, phone: phone, email: email, kota: kota, jabatan: jabatan, pesan: pesan});
 		posting.done(function(data){
 			if(data === "success"){
 				$form.find('input[name="name"]').val("");
@@ -63,12 +64,13 @@ $('#form_kontak').submit(function(e){
 				$form.find('input[name="phone"]').val("");
 				$form.find('input[name="email"]').val("");
 				$form.find('input[name="kota"]').val("");
-				$form.find('select[name="provinsi"]>option[value="34"]').prop("selected", true);
-				alert("Data Anda Telah Tersimpan. Terimakasih.");
+				$form.find('input[name="jabatan"]').val("");
+				$form.find('input[name="pesan"]').val("");
+				$.growl.notice({ message: "Data Berhasil Di Input. Terimakasih!" });
 			} else if (data === "connection_error"){
-				alert("internal server error");
+				$.growl.warning({ message: "Internal Server Error!" });
 			}else{
-				alert("unknown error");
+				$.growl.warning({ message: "Unknown Error" });
 			}
 			console.log(data);
 		});
